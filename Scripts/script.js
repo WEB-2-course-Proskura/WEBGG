b51f2e8ed6ce3947df2009838428ef48ecaea86f
Hurmaze <72215361+Hurmaze@users.noreply.github.com>
2021-11-08 19:34:38 +0200

    щось зроблено

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,12 @@
-let where = document.getElementById('linkForLab4');
+/*              Script for lab4
+et where = document.getElementById('linkForLab4');
 where.innerHTML = '<a href="">Якийсь цікавий слоган</a>';
 
 function ClickH1(id){
     let click = document.getElementById(id);
     click.style.color = 'red';
-}
+}*/
+
+
 
 
